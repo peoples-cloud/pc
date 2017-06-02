0012e50701949483184153c726ddd865eec52c32
cblgh <cblgh@cblgh.org>
2017-06-02 14:46:04 +0200

    rename methods

--- tar/tar.go
+++ tar/tar.go
@@ -6,7 +6,7 @@ import (
 )
 
 // TODO: find solution that doesn't depend on os environemnt i.e. entirely golang based solution for tar unpack & pack
-func UnpackTar(tarball, dest string) {
+func Unpack(tarball, dest string) {
 	cmd := exec.Command("tar", "xf", tarball, "-C", dest, ".")
 	fmt.Printf("unpacking %s into %s\n", tarball, dest)
 	err := cmd.Run()
@@ -15,7 +15,7 @@ func UnpackTar(tarball, dest string) {
 	}
 }
 
-func PackTar(src, tarball string) {
+func Pack(src, tarball string) {
 	cmd := exec.Command("tar", "czf", tarball, "-C", src, ".")
 	err := cmd.Run()
 	if err != nil {
